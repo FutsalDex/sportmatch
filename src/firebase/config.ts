028c264677d8cd5f47d48a6720e5344713a6b213
FutsalDex <futsaldex@gmail.com>
2026-05-06 16:53:19 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2816733526-e6799",
+  "appId": "1:95364224816:web:e6e000a564be2fdf959af5",
+  "apiKey": "AIzaSyD4gG37H_55_lNBETy3fEi1QsUj_nGlIsQ",
+  "authDomain": "studio-2816733526-e6799.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "95364224816"
+};
